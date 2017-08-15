b2487e8ac4aea0083d6e76b08e0c2daae55be665
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-08-15 15:50:12 +0900

    Added Enable-SPClientFeature function

---
+++ source/SPClient.Tests/functions/SPClientFeature/Enable-SPClientFeature.Tests.ps1
@@ -0,0 +1,77 @@
+﻿#Requires -Version 3.0
+
+. "$($PSScriptRoot)\..\..\TestInitialize.ps1"
+
+Describe 'Enable-SPClientFeature' {
+
+    Context 'Success' {
+
+        Context 'Site Collection Features' {
+
+            BeforeEach {
+                try {
+                    $Site = $SPClient.ClientContext.Site
+                    $Site.Features.Remove($SPClient.TestConfig.SiteFeatureId, $true)
+                    $SPClient.ClientContext.ExecuteQuery()
+                } catch {
+                    Write-Host $_ -ForegroundColor Yellow 
+                }
+            }
+
+            It 'Enables a feature' {
+                $Params = @{
+                    Identity = $SPClient.TestConfig.SiteFeatureId
+                    PassThru = $true
+                }
+                $Result = Enable-SPClientFeature @Params
+                $Result | Should Not BeNullOrEmpty
+                $Result | Should BeOfType 'Microsoft.SharePoint.Client.Site'
+            }
+
+        }
+
+        Context 'Site Features' {
+
+            BeforeEach {
+                try {
+                    $Web = $SPClient.ClientContext.Site.OpenWebById($SPClient.TestConfig.WebId)
+                    $Web.Features.Remove($SPClient.TestConfig.WebFeatureId, $true)
+                    $SPClient.ClientContext.ExecuteQuery()
+                } catch {
+                    Write-Host $_ -ForegroundColor Yellow 
+                }
+            }
+
+            It 'Enables a feature' {
+                $Web = $SPClient.ClientContext.Site.OpenWebById($SPClient.TestConfig.WebId)
+                $Params = @{
+                    ParentObject = $Web
+                    Identity = $SPClient.TestConfig.WebFeatureId
+                    PassThru = $true
+                }
+                $Result = Enable-SPClientFeature @Params
+                $Result | Should Not BeNullOrEmpty
+                $Result | Should BeOfType 'Microsoft.SharePoint.Client.Web'
+            }
+
+        }
+
+    }
+
+    Context 'Failure' {
+
+        It 'Throws an error when the feature could not be found by id' {
+            $Throw = {
+                $Web = $SPClient.ClientContext.Site.OpenWebById($SPClient.TestConfig.WebId)
+                $Params = @{
+                    Identity = '84370E78-AD1B-469E-8068-8B7303C8A55B'
+                }
+                $Result = Enable-SPClientFeature @Params
+                $Result | Should Not BeNullOrEmpty
+            }
+            $Throw | Should Throw 'The specified feature could not be found.'
+        }
+
+    }
+
+}
---
+++ source/SPClient/functions/SPClientFeature/Enable-SPClientFeature.ps1
@@ -0,0 +1,75 @@
+﻿#Requires -Version 3.0
+
+<#
+  Get-SPClientFeature.ps1
+
+  Copyright (c) 2017 karamem0
+
+  This software is released under the MIT License.
+  https://github.com/karamem0/SPClient/blob/master/LICENSE
+#>
+
+function Enable-SPClientFeature {
+
+<#
+.SYNOPSIS
+  Enables a feature.
+.DESCRIPTION
+  The Enable-SPClientFeature function enables a site collection feature or a site feature.
+.PARAMETER ClientContext
+  Indicates the client context. If not specified, uses default context.
+.PARAMETER ParentObject
+  Indicates the site collection or site to which the feature to be enabled.
+.PARAMETER Identity
+  Indicates the feature GUID.
+.PARAMETER PassThru
+  If specified, returns input object.
+.EXAMPLE
+  GEnable-SPClientFeature $web -Identity "99FE402E-89A0-45AA-9163-85342E865DC8"
+.INPUTS
+  None or SPClient.SPClientFeatureParentPipeBind
+.OUTPUTS
+  None
+.LINK
+  https://github.com/karamem0/SPClient/blob/master/doc/Enable-SPClientFeature.md
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [Microsoft.SharePoint.Client.ClientContext]
+        $ClientContext = $SPClient.ClientContext,
+        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
+        [SPClient.SPClientFeatureParentPipeBind]
+        $ParentObject = $ClientContext.Site,
+        [Parameter(Mandatory = $true)]
+        [Alias('Id')]
+        [string]
+        $Identity,
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $PassThru
+    )
+
+    process {
+        if ($ClientContext -eq $null) {
+            throw "Cannot bind argument to parameter 'ClientContext' because it is null."
+        }
+        if ($ParentObject -eq $null) {
+            throw "Cannot bind argument to parameter 'ParentObject' because it is null."
+        }
+        $ClientObject = $ParentObject.ClientObject
+        $ClientObject.Features.Add($Identity, $true, 0) | Out-Null
+        Invoke-ClientContextLoad `
+            -ClientContext $ClientContext `
+            -ClientObject $ClientObject `
+            -Retrieval 'Features'
+        if ($PassThru) {
+            Write-Output $ClientObject
+        }
+        trap {
+            throw 'The specified feature could not be found.'
+        }
+    }
+
+}
