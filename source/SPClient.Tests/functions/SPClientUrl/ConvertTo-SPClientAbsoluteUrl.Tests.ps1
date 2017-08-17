757e6e4829257599c4bc97d844820b7cc05923be
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-08-17 17:21:51 +0900

    Added ConvertTo-SPClientAbsoluteUrl function

---
+++ source/SPClient.Tests/functions/SPClientUrl/ConvertTo-SPClientAbsoluteUrl.Tests.ps1
@@ -0,0 +1,33 @@
+﻿#Requires -Version 3.0
+
+. "$($PSScriptRoot)\..\..\TestInitialize.ps1"
+
+Describe 'ConvertTo-SPClientAbsoluteUrl' {
+
+    Context 'Success' {
+
+        It 'Converts a relative url to a absolute url' {
+            $AbsoluteUrl = $SPClient.TestConfig.RootUrl + $SPClient.TestConfig.ListUrl
+            $RelativeUrl = $SPClient.TestConfig.ListUrl
+            $Params = @{
+                Url = $RelativeUrl
+            }
+            $Result = ConvertTo-SPClientAbsoluteUrl @Params
+            $Result | Should Not BeNullOrEmpty
+            $Result | Should Be $AbsoluteUrl
+        }
+
+        It 'Converts a absolute url to a absolute url' {
+            $AbsoluteUrl = $SPClient.TestConfig.RootUrl + $SPClient.TestConfig.ListUrl
+            $RelativeUrl = $SPClient.TestConfig.ListUrl
+            $Params = @{
+                Url = $AbsoluteUrl
+            }
+            $Result = ConvertTo-SPClientAbsoluteUrl @Params
+            $Result | Should Not BeNullOrEmpty
+            $Result | Should Be $AbsoluteUrl
+        }
+
+    }
+
+}
