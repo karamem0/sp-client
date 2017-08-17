0ae1e167b0e24f4575c5563dffb079d54b333102
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-08-17 17:24:32 +0900

    Added ConvertTo-SPClientRelativeUrl function

---
+++ source/SPClient.Tests/functions/SPClientUrl/ConvertTo-SPClientRelativeUrl.Tests.ps1
@@ -0,0 +1,33 @@
+﻿#Requires -Version 3.0
+
+. "$($PSScriptRoot)\..\..\TestInitialize.ps1"
+
+Describe 'ConvertTo-SPClientRelativeUrl' {
+
+    Context 'Success' {
+
+        It 'Converts a absolute url to a relative url' {
+            $AbsoluteUrl = $SPClient.TestConfig.RootUrl + $SPClient.TestConfig.ListUrl
+            $RelativeUrl = $SPClient.TestConfig.ListUrl
+            $Params = @{
+                Url = $AbsoluteUrl
+            }
+            $Result = ConvertTo-SPClientRelativeUrl @Params
+            $Result | Should Not BeNullOrEmpty
+            $Result | Should Be $RelativeUrl
+        }
+
+        It 'Converts a relative url to a relative url' {
+            $AbsoluteUrl = $SPClient.TestConfig.RootUrl + $SPClient.TestConfig.ListUrl
+            $RelativeUrl = $SPClient.TestConfig.ListUrl
+            $Params = @{
+                Url = $RelativeUrl
+            }
+            $Result = ConvertTo-SPClientRelativeUrl @Params
+            $Result | Should Not BeNullOrEmpty
+            $Result | Should Be $RelativeUrl
+        }
+
+    }
+
+}
