bd9e17bcd6cb002a774045cb7b3e373258cc316d
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-08-17 11:57:17 +0900

    Refactored SPClientFieldPipeBind class

--- source/SPClient/functions/SPClientListItem/Get-SPClientListItem.ps1
+++ source/SPClient/functions/SPClientListItem/Get-SPClientListItem.ps1
@@ -78,7 +78,7 @@ function Get-SPClientListItem {
         [string]
         $Scope,
         [Parameter(Mandatory = $false, ParameterSetName = 'All')]
-        [SPClient.SPClientFieldIdentityPipeBind[]]
+        [SPClient.SPClientFieldPipeBind[]]
         $ViewFields,
         [Parameter(Mandatory = $false, ParameterSetName = 'All')]
         [string]
@@ -123,7 +123,7 @@ function Get-SPClientListItem {
                 $ViewFieldsElement = $ViewElement.AppendChild($XmlDocument.CreateElement('ViewFields'))
                 foreach ($ViewField in $ViewFields) {
                     $FieldRefElement = $ViewFieldsElement.AppendChild($XmlDocument.CreateElement('FieldRef'))
-                    $FieldRefElement.SetAttribute('Name', $ViewField.GetValue($ClientContext))
+                    $FieldRefElement.SetAttribute('Name', $ViewField.GetField($ParentObject.ClientObject).InternalName)
                 }
             }
             if ($PSBoundParameters.ContainsKey('Query')) {
---
+++ source/SPClient/types/SPClientFieldPipeBind.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * SPClientFieldPipeBind.cs
+ *
+ * Copyright (c) 2017 karamem0
+ *
+ * This software is released under the MIT License.
+ * https://github.com/karamem0/SPClient/blob/master/LICENSE
+ */
+
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPClient {
+
+    /// <summary>
+    /// Represents a parameter that stores a column.
+    /// </summary>
+    public class SPClientFieldPipeBind {
+
+        /// <summary>
+        /// Indicates the column GUID.
+        /// </summary>
+        private Guid fieldId;
+
+        /// <summary>
+        /// Indicates the column title or internal name.
+        /// </summary>
+        private string fieldName;
+
+        /// <summary>
+        /// Indicates the column.
+        /// </summary>
+        private Field field;
+
+        /// <summary>
+        /// Initializes the new instance of <see cref="SPClient.SPClientFieldPipeBind"/> class.
+        /// </summary>
+        /// <param name="fieldId">the column GUID.</param>
+        public SPClientFieldPipeBind(Guid fieldId) {
+            if (fieldId == Guid.Empty) {
+                throw new ArgumentNullException("fieldId");
+            }
+            this.fieldId = fieldId;
+        }
+
+        /// <summary>
+        /// Initializes the new instance of <see cref="SPClient.SPClientFieldPipeBind"/> class.
+        /// </summary>
+        /// <param name="fieldName">the column title or internal name.</param>
+        public SPClientFieldPipeBind(string fieldName) {
+            if (fieldName == null) {
+                throw new ArgumentNullException("fieldName");
+            }
+            var fieldId = Guid.Empty;
+            if (Guid.TryParse(fieldName, out fieldId) == true) {
+                this.fieldId = fieldId;
+            } else {
+                this.fieldName = fieldName;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the new instance of <see cref="SPClient.SPClientFieldPipeBind"/> class.
+        /// </summary>
+        /// <param name="field">the column.</param>
+        public SPClientFieldPipeBind(Field field) {
+            if (field == null) {
+                throw new ArgumentNullException("field");
+            }
+            this.field = field;
+        }
+
+        /// <summary>
+        /// Returns the column.
+        /// </summary>
+        /// <param name="list">the parent list.</param>
+        public Field GetField(List list) {
+            if (this.field == null && this.fieldId != Guid.Empty) {
+                this.field = list.Fields.GetById(this.fieldId);
+                list.Context.Load(this.field);
+                list.Context.ExecuteQuery();
+            }
+            if (this.field == null && this.fieldName != null) {
+                this.field = list.Fields.GetByInternalNameOrTitle(this.fieldName);
+                list.Context.Load(this.field);
+                list.Context.ExecuteQuery();
+            }
+            return this.field;
+        }
+
+    }
+
+}
