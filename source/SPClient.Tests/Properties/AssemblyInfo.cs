8390a14a300749995bc4c5b3d3cc3dfca15f1efb
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-09-01 11:44:14 +0900

    Added solution files

---
+++ source/SPClient.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: AssemblyTitle("SPClient.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("karamem0")]
+[assembly: AssemblyProduct("SPClient")]
+[assembly: AssemblyCopyright("Copyright © 2017 karamem0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
